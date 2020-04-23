27bb1baf6b3a2062697d5cb1aad527f7809e018d
Kristofer Baxter <kristofer@kristoferbaxter.com>
2020-04-23 12:11:53 -0700

    Only commit when there are modifications

--- start.sh
+++ start.sh
@@ -13,20 +13,29 @@ REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
     exit 1;
 };
 
-echo "Commit to branch $INPUT_BRANCH";
+_git_is_dirty() {
+    [ -n "$(git status -s)" ]
+}
 
+if _git_is_dirty; then
+    echo "Commit to branch $INPUT_BRANCH";
+    git add .
+    git commit -m "Pushed Changes from Github Actions Bot"
 
-echo "Push to branch $INPUT_BRANCH";
-if ${INPUT_FORCE}; then
-    _FORCE_OPTION='--force'
+    echo "Push to branch $INPUT_BRANCH";
+    if ${INPUT_FORCE}; then
+        _FORCE_OPTION='--force'
+    fi
+
+    if ${TAGS}; then
+        _TAGS='--tags'
+    fi
+
+    cd ${INPUT_DIRECTORY}
+
+    remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+
+    git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
+else
+    echo "Working tree clean. Nothing to commit."
 fi
-
-if ${TAGS}; then
-    _TAGS='--tags'
-fi
-
-cd ${INPUT_DIRECTORY}
-
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
-
-git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
